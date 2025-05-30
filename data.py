5935b624b25a4724450b001282a55c002da6dd76
Aaron Belikoff <aaronbelikoff@gmail.com>
2025-05-30 12:22:34 -0400

    add new formula-based solver

--- data.py
+++ data.py
@@ -1,20 +1,49 @@
 import json
+import random
 
-CLADDER_PATH = "data/cladder-v1-q-balanced.json"
-
-with open(CLADDER_PATH, "r", encoding="utf-8") as f:
-    data = json.load(f)
 
 def pretty_print(example):
     text = f"Question ID: {example['question_id']}\n"
+    text += f"Description ID: {example['desc_id']}\n"
     text += f"Given Info: {example['given_info'].strip()}\n"
     text += f"Question: {example['question'].strip()}\n"
-    text += "Reasoning:\n"
-    for k, v in example['reasoning'].items():
-        text += f"  {k.strip()}: {v.strip()}\n"
-    text += f"Answer: {example['answer'].strip()}"
+    text += f"Answer: {example['answer'].strip()}\n"
+    if example["reasoning"]:
+        text += "Reasoning:\n"
+        for k, v in example["reasoning"].items():
+            text += f"  {k.strip()}: {v.strip()}\n"
+    text += f"Meta: {example['meta']}"
     print(f"\n{text}\n")
 
-for example in data:
-    input("Press Enter to show example.")
-    pretty_print(example)
+
+def iterate(data):
+    for example in data:
+        input("Press Enter to show example.")
+        pretty_print(example)
+
+
+def find_equations(data):
+    all_items = set()
+
+    for example in data:
+        item = f"{example['meta']['query_type']} {example['meta']['formal_form']}"
+        if item not in all_items:
+            print(item)
+            all_items.add(item)
+
+    print(f"Processed {len(data):,} items")
+    print(f"Found {len(all_items):,} unique (query_type, formal_form) pairs")
+
+
+if __name__ == "__main__":
+    CLADDER_PATH = "data/cladder-v1-q-balanced.json"
+    RANDOMIZE = True
+
+    with open(CLADDER_PATH, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    if RANDOMIZE:
+        random.shuffle(data)
+
+    find_equations(data)
+    # iterate(data)
